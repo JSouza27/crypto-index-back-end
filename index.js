becfef0e170e7d3df4f046367fd151e0ef24df00
Jonathan Souza <souzajonathan570@gmail.com>
2022-02-14 16:59:04 -0300

    feat: add sequelize settings

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (_req, res) => {
+  res.json('funcionando');
+});
+
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
