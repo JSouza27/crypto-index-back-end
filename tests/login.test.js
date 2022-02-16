cbf4559feace8d7dad9a5c92139f1c8aa4e82272
Jonathan Souza <souzajonathan570@gmail.com>
2022-02-16 17:57:35 -0300

    feat: add teste route login

--- tests/login.test.js
+++ tests/login.test.js
@@ -34,7 +34,61 @@ describe('POST /api/login', () => {
     });
   });
 
-  describe('Quando email ou senha inválida', () => {});
+  describe('Quando email ou senha inválida', () => {
+    let response;
 
-  describe('Quando o login é feito com sucesso', () => {});
+    before(async () => {
+      response = await chai.request(server)
+        .post('/api/login')
+        .send({
+          "email": "emailmail.com",
+          "password": "1359820"
+        })
+      });
+
+    it('retorna código de status 400', () => {
+      expect(response).to.have.status(HttpStatus.badRequest);
+    });
+
+    it('retorna um objeto', () => {
+      expect(response.body).to.be.an('object');
+    });
+
+    it('o objeto de resposta possui a propriedade "message"', () => {
+      expect(response.body).to.have.property('message');
+    });
+
+    it('a propriedade "message" tem o valor "Campos inválidos"', () => {
+      expect(response.body.message).to.be.equals('Campos inválidos');
+    });
+  });
+
+  describe('Quando o login é feito com sucesso', () => {
+    let response;
+
+    before(async () => {
+      response = await chai.request(server)
+        .post('/api/login')
+        .send({
+          "email": "email@mail.com",
+          "password": "135982"
+        })
+    });
+      
+    it('retorna código de status 400', () => {
+      expect(response).to.have.status(HttpStatus.ok);
+    });
+
+    it('retorna um objeto', () => {
+      expect(response.body).to.be.an('object');
+    });
+
+    it('o objeto de resposta possui a propriedade "token"', () => {
+      expect(response.body).to.have.property('token');
+    });
+
+    it('o valor da propriedade "token" tem o tamanho de 16 caracteres ', () => {
+      expect(response.body.token).to.have.length(16);
+    });
+  });
 });
