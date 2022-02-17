689203f62ca28be11d0a0be96661db839c4c106d
Jonathan Souza <souzajonathan570@gmail.com>
2022-02-17 14:23:35 -0300

    feat: add cors library

--- src/api/app.js
+++ src/api/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('../utils/swagger.json');
 const login = require('../router/loginRouter');
 const exchange = require('../router/exchangeRoute');
 const { nonExistentRoute } = require('../middlewares/notFoundMiddleware');
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get('/api', (_req, res) => {
   res.json({ message: 'Bem vindo a Crypto Index API!' });
